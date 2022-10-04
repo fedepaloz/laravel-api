d87a9ecff26c18cc7ae904c2717223038095e5d2
fedepaloz <palozzifederico@gmail.com>
2022-10-04 14:43:12 +0200

    init

---
+++ resources/js/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="content">
+    <Placeholder :title="title"></Placeholder>
+    <div class="title m-b-md"></div>
+  </div>
+</template>
+
+ 
+ <script>
+import Placeholder from "./Placeholder.vue";
+export default {
+  name: "App",
+  components: {
+    Placeholder,
+  },
+  data() {
+    return {
+      title: "WIP",
+    };
+  },
+};
+</script>
+
+
+ 
